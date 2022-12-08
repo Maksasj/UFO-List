6789b98d3f80798b17205c69087d0863f252d5fa
maksasj <radioboos@gmail.com>
2022-12-08 21:42:01 +0200

    Basic function prototypes

--- include/chashmap/chashmap.h
+++ include/chashmap/chashmap.h
@@ -1,8 +1,52 @@
 #ifndef CHASHMAP_H
 #define CHASHMAP_H
 
-#include <stdlib.h>
+typedef struct HashmapEntry {
+    char* key;
+    void* value;
 
-void ctest();
+    /* Pointer to next element */
+    HashmapEntry* next;
+} HashmapEntry;
+
+typedef struct Hashmap {
+    /* Amount of elements in HashmapEntry*/
+    const long segment_length;
+
+    /* Actual array of hashmap entries */
+    void* HashmapEntry;
+
+    /* Byte size */
+    size_t size;
+
+    /* Amount of elements */
+    size_t length;
+} Hashmap;
+
+void mapInit(Hashmap* map);
+
+void* mapInsert(Hashmap* map, char* key, char* value);
+
+void mapGetValue(Hashmap* map, char* key);
+
+int mapKeyExist(Hashmap* map, char* key);
+
+void mapFree(Hashmap* map);
+
+void chashmap_hash(const char* key);
+
+/*
+Hashmap hm;
+mapInit(&hm);
+    mapInsert(&hm, "key1", "value1");
+    mapInsert(&hm, "key2", "value2");
+    mapInsert(&hm, "key3", "value3");
+mapFree()
+
+char* value = mapGet(&hm, "key1");
+
+mapKeyExist(&hm, "key2");
+
+*/
 
 #endif
